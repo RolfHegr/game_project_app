f4a5857ebc918bff9c601486d5b3b423be47013e
Rolf <rolfhegrand@gmail.com>
2022-04-13 15:47:59 +0300

    FE pages, login, signup, games + basic BE

---
+++ gaming_app_be/src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+//makes it possible to fetch and post to and from localhost
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+//basic variable to verify login
+const user = {
+  name: "test",
+  password: "123",
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/login", (req, res) => {
+  res.send(`[${user.name},${user.password}]`);
+});
+
+app.listen(PORT, () => {
+  console.log("App listening on port", PORT);
+});
