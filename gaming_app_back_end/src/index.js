8ea7f4de73db5db3b932b36ba88909629c8cbab0
Rolf <rolfhegrand@gmail.com>
2022-04-17 18:43:13 +0300

    Middleware route not found added

--- gaming_app_back_end/src/index.js
+++ gaming_app_back_end/src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
-import loginRoute from "./routes/loginRoute.js"
-import signupRoute from "./routes/signupRoute.js"
+import loginRoute from "./routes/loginRoute.js";
+import signupRoute from "./routes/signupRoute.js";
+import mysql from "mysql";
+import notFoundMiddleware from "./middleware/not-found.js";
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+
+//Middleware
+notFoundMiddleware
 
 //makes it possible to fetch and post to and from localhost
 app.use((req, res, next) => {
@@ -15,11 +20,13 @@ app.use((req, res, next) => {
 
 app.get("/login", loginRoute);
 
-app.post("/signup", signupRoute)
+app.post("/signup", signupRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use(notFoundMiddleware );
 
 app.listen(PORT, () => {
   console.log("App listening on port", PORT);
