4c6322a8ae30d21391aa84bdf10e8a14b478a48a
Rolf <rolfhegrand@gmail.com>
2022-04-17 19:11:33 +0300

    added .env

--- gaming_app_back_end/src/index.js
+++ gaming_app_back_end/src/index.js
@@ -1,16 +1,20 @@
+import 'dotenv/config'
 import express from "express";
+import mysql from "mysql";
+
+
+//Routes
 import loginRoute from "./routes/loginRoute.js";
 import signupRoute from "./routes/signupRoute.js";
-import mysql from "mysql";
+
+//Middleware
 import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-
-//Middleware
-notFoundMiddleware
 
 //makes it possible to fetch and post to and from localhost
 app.use((req, res, next) => {
@@ -26,8 +30,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use(notFoundMiddleware );
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
   console.log("App listening on port", PORT);
 });
+ 
