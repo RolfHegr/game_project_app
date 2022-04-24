d1b5018affb05401a651e6e818ce060a7f7356d3
Rolf <rolfhegrand@gmail.com>
2022-04-24 10:06:03 +0300

    fixed BE error, apparently my changes from yesterday wasnt included in yesterdays commit

--- gaming_app_back_end/src/controllers/scores.js
+++ gaming_app_back_end/src/controllers/scores.js
@@ -4,10 +4,12 @@ import { StatusCodes } from "http-status-codes";
 
 const lastScore = async (req, res) => {
   const { email } = req.body;
+  console.log("res.data", res.data);
 
   try {
     const noScore = "User have no scores saved";
     const user = await User.findOne({ email });
+    console.log("user", user);
     if (!user.highScoreCandy.score) {
       res.status(StatusCodes.OK).json({
         noScore,
@@ -16,9 +18,11 @@ const lastScore = async (req, res) => {
 
     const { newScores } = user.highScoreCandy;
     const lastScore = newScores[newScores.length - 1];
+    const date = user.highScoreCandy.date
 
     res.status(StatusCodes.OK).json({
       lastScore,
+      date
     });
   } catch (error) {
     console.error(error);
--- gaming_app_back_end/src/routes/scoreRoutes.js
+++ gaming_app_back_end/src/routes/scoreRoutes.js
@@ -4,7 +4,7 @@ import { updateScore, lastScore, highScore } from "../controllers/scores.js";
 const router = express.Router();
 
 router.route("/updateScore").post(updateScore);
-router.route("/lastScore").get(lastScore);
-router.route("/highScore").get(highScore);
+router.route("/lastScore").post(lastScore);
+router.route("/highScore").post(highScore);
 
 export default router;
